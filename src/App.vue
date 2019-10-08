717a36ac79a83f636cc5b5ee743b1af2385a6920
shubhambattoo <shubham.battoo@hotmail.com>
2019-10-08 23:40:11 +0530

    created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,50 @@
 <template>
-  <div>
-    <router-view/>
+  <div id="app" class="light">
+    <router-view />
   </div>
 </template>
 
 <style>
+:root {
+  --light-bg-color: #f2f3f4;
+  --dark-bg-color: #0f0f0f;
+  --default-bg-color: var(--light-bg-color);
+  --light-color: #555;
+  --dark-color: #f5f5f5;
+  --default-color: var(--light-color);
+  --darker-bg-color : #000;
+  --white-bg-color : #fff;
+  --nav-bg-color : var(--white-bg-color);
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-size: 1.6rem;
+}
+
+#app {
+  background-color: var(--default-bg-color);
+  height: 100vh;
+  color: var(--default-color);
+}
+
+#app.light {
+  --default-bg-color: var(--light-bg-color);
+  --default-color: var(--light-color);
+}
+
+#app.dark {
+  --default-bg-color: var(--dark-bg-color);
+  --default-color: var(--dark-color);
+}
+
+.container {
+  max-width: 90rem;
+  margin: 0 auto;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,24 @@
 <template>
   <header>
+    <div class="container">
+      <nav class="nav">
+        <div class="brand">
+          <router-link to="/">
+            <img src="//via.placeholder.com/50" alt="brand image" />
+          </router-link>
+        </div>
+        <div class="menu">
+          <ul>
+            <li class="icon paw">
+              <router-link to="/">Breeds</router-link>
+            </li>
+            <li class="icon heart">
+              <router-link to="/">Likes</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -10,4 +29,56 @@ export default {
 </script>
 
 <style>
+header {
+  background-color: var(--nav-bg-color);
+  padding: 2rem;
+}
+.nav {
+  display: flex;
+}
+.menu {
+  margin-left: auto;
+  display: flex;
+}
+
+.menu ul {
+  list-style-type: none;
+  display: flex;
+}
+
+.menu ul li:not(:last-child) {
+  margin-right: 4.4rem;
+}
+
+.menu ul li {
+  position: relative;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.menu ul li.icon::before {
+  content: "";
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: left;
+  position: absolute;
+  height: 1.5rem;
+  left: -2.2rem;
+  width: 1.5rem;
+}
+
+.menu ul li.icon.paw::before {
+  background-image: url("../assets/paw-print-.png");
+}
+
+.menu ul li.icon.heart::before {
+  background-image: url("../assets/heart.png");
+}
+
+.menu ul li a {
+  color: var(--default-color);
+  text-decoration: none;
+  text-transform: uppercase;
+}
 </style>
